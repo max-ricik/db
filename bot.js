c0fe1eb272b8bf35cca5c9a4d557b979cd09b087
max-ricik <55506811+max-ricik@users.noreply.github.com>
2019-09-18 21:21:00 +0200

    s

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 var Discord = require('discord.io');
 var logger = require('winston');
-var token = process.env.token;
+var auth = process.env.token;
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
